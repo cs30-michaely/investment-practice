27f3239c04191ccc94b736614f6b35e2f9bc74ad
Michael-Yakimchuk <38264467+Michael-Yakimchuk@users.noreply.github.com>
2018-05-11 15:32:32 -0600

    Add files via upload

---
+++ investment/header.php
@@ -0,0 +1,15 @@
+<head>
+  <link rel="stylesheet" type="text/css" href="myStyle.css">
+    
+</head>
+
+<body>
+  <header>
+    <img id="logo" src="img/companyLogo.jpg">
+    <ul>
+      <li>Products</li>
+      <li>Staff</li>
+      <li>Tips and Tricks</li>
+      <li>About Us</li>
+    </ul>
+  </header>
---
+++ investment/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+
+
+<?php include 'header.php';?>
+</body>
+
+</html>
